36389520bed1fcad6020febb1bbb68eece0a3a46
p28Gamaliel <gamaliel13perez@gmail.com>
2025-08-10 23:43:55 +0000

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - ordena un arreglo de enteros de forma ascendente
+ * @array: arreglo de enteros a ordenas
+ * @size: tamano del arreglo
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0 ;
+
+		for (j = 0; j < size -  1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
+
+				swapped = 1;
+			}
+		}
+
+		if (!swapped)
+			break;
+	}
+}
